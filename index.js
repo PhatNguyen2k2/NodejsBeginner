90ba43175d3fedd33d8c518c4ff6b7f7ac8fea80
phatnguyen02 <ngphat1211@gmail.com>
2022-01-16 03:18:56 +0700

    tìm hiểu url

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var http = require('http');
+var url = require('url');
 var myDate = require('./myDate');
 http.createServer(function(req,res){
     //res.writeHead(200, {'Content-type':'text/plain'});
-    res.end(myDate.myDateTime());
+    // res.write(req.url);
+    //res.end(myDate.myDateTime());
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end(txt);
 }).listen(8081);
