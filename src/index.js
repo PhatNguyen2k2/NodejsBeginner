86309c5a4582c88a4a6d26101da9d47e740feab1
Phat Nguyen <ngphat1211@gmail.com>
2022-06-09 00:01:53 +0700

    learning

--- src/index.js
+++ src/index.js
@@ -9,12 +9,28 @@
 //     var txt = q.year + " " + q.month;
 //     res.end(txt);
 // }).listen(8081);
+const path = require('path')
 const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
+//http logger
+app.use(express.static(path.join(__dirname, 'resources\\public'))) //static files
+app.use(morgan("combined"))
+//template engine
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views',path.join(__dirname, 'resources\\views'))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
+})
+app.get('/news', (req, res) => {
+  res.render('news')
 })
 
 app.listen(port, () => {
